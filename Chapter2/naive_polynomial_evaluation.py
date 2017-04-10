f4307a8747b0652dce9eb116ebf98962daeb80a2
lee <ljx_199463@163.com>
2017-04-10 22:49:24 +0800

    Chapter 2 and Chapter 3 over

---
+++ Chapter2/naive_polynomial_evaluation.py
@@ -0,0 +1,28 @@
+
+import doctest
+
+def polynomial_evaluation(a,x):
+    """1*1 + 5 * 2 + 3 * 4 + 2 * 8 = 1 + 10 + 12 + 16 = 23 + 16 = 39
+    Example
+    -------
+    >>> polynomial_evaluation('1 5 3 2',2)
+    39
+    """
+    a=a.split()
+    _sum = 0
+    last_x_i = 1
+    for i in xrange(0,len(a)):
+        if i != 0:
+            x_i = x * last_x_i
+        else:
+            x_i = last_x_i
+        _sum = _sum + int(a[i]) * x_i
+        last_x_i = x_i
+    return _sum
+
+def main():
+    print polynomial_evaluation('1 5 3 2',2)
+
+
+if __name__ == '__main__' :
+    main()
